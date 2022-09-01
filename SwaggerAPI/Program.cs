0d1f9bacec9d20d92381223c9869bd88da2c1607
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-09-01 16:05:02 +0300

    Für Swagger Middleware hinzufügen

--- SwaggerAPI/Program.cs
+++ SwaggerAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SwaggerAPI.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,29 @@ builder.Services.AddDbContext<TestDbContext>(opt =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Produkt API",
+        Description = "Eine ASP.NET Core-Web-API zum Verwalten von Produkt-Elementen",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Kontakt",
+            Url = new Uri("https://github.com/AliRamazanYildirim")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Lizenz",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
@@ -19,7 +43,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Produkt API");
+    });
 }
 
 app.UseHttpsRedirection();
